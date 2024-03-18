d1b4a3752b0e84412724eb5a5c3147e7f3560763
eric <ericmaina11455@gmail.com>
2024-03-18 10:46:11 +0300

    adding-arithmetic-functions

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b){
+    return;
+}
+function subtract(a,b){
+    return;
+}
+function multiply(a,b){
+    return;
+}
+function divide(a,b){
+    return;
+}
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(n){
+    n +=1;
+    return n;
+}
+function decrement(n){
+    n -=1;
+    return n;
+}
+function makeInt(n){
+    return parseInt(n,10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
